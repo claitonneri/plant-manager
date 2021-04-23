816c191dac41a7bc4b59f45b389f763a58556b14
Claiton Neri <claitondidone@gmail.com>
2021-04-23 00:24:13 -0300

    tela welcome: estrutura geral realizada

---
+++ src/@types/index.d.ts
@@ -0,0 +1,4 @@
+declare module '*.png' {
+  const content: never;
+  export default content;
+}
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+import wateringImg from '../assets/watering.png';
+
+import colors from '../styles/colors';
+
+const Welcome: React.FC = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {'\n'}
+        suas plantas de {'\n'}
+        forma fácil
+      </Text>
+
+      <Image source={wateringImg} resizeMode="contain" style={styles.image} />
+
+      <Text style={styles.subtitle}>
+        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+        sempre que precisar.
+      </Text>
+
+      <TouchableOpacity activeOpacity={0.6} style={styles.button}>
+        <Feather name="chevron-right" style={styles.buttonIcon} />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.heading,
+    marginTop: 38,
+  },
+  subtitle: {
+    textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+  image: {
+    height: Dimensions.get('window').width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 32,
+  },
+});
+
+export default Welcome;
